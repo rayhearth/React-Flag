65178f1717298dd6ab81b8352187e683ec4199c0
Emmanuelle Jupon <emmanuellejupon@gmail.com>
2022-11-01 11:11:11 +0100

    cours-react-flag

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>Apropos</h1>
+            <br />
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere velit eum maxime quos dolore, sapiente dolor quasi architecto, inventore repudiandae beatae magni, facilis animi quisquam commodi autem cum id. Magnam eius amet reprehenderit sint distinctio! Adipisci quisquam voluptates dolores quidem exercitationem eos incidunt, numquam, accusantium sapiente quos expedita! Cupiditate facere ab, ducimus voluptates hic, omnis placeat dolore amet nulla unde saepe nam veritatis? Obcaecati, repudiandae deleniti consequatur fuga dignissimos ab est sit odio culpa error dolore exercitationem hic ratione mollitia aspernatur sapiente animi recusandae quod quaerat! Delectus quos rem expedita excepturi, ad in, laudantium adipisci ullam cumque voluptatem impedit esse.</p>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur necessitatibus aspernatur sit adipisci alias culpa error nisi, voluptates, laborum quae libero magnam natus quam porro, rerum et eos molestiae sapiente non laboriosam rem! Veniam odit quo aperiam eos ipsa. Eius dolores sapiente, quia adipisci vel necessitatibus dolor dignissimos tenetur natus quod totam fugiat reiciendis harum corrupti impedit ipsam reprehenderit molestias nisi tempore esse quaerat ad, non ipsa sunt. Maiores accusamus quasi eos quam quod. Repellendus est accusantium fuga, assumenda inventore, omnis itaque enim delectus mollitia consectetur tempora obcaecati eligendi nulla corrupti. Rerum, molestias. Nemo quo rerum est magni maiores minima facilis neque amet? In, iure voluptates? Libero quia perferendis corporis consequuntur, expedita, officiis modi repellat dolores natus, eos quibusdam. Nisi aliquid porro beatae et quidem dicta modi consequatur dolore, hic molestiae perspiciatis. Voluptatibus nulla, fugiat tempora molestias, quaerat minus aliquam accusamus assumenda ipsam perspiciatis doloremque itaque iure! Hic possimus tempore rerum earum, perferendis error quae expedita voluptate ex excepturi sapiente odit vel dolorum officiis reprehenderit incidunt mollitia similique veritatis, placeat quis. Rem, optio similique placeat quos sapiente perspiciatis consectetur doloremque, eligendi voluptate voluptatum debitis cum ullam eos veniam commodi pariatur laboriosam vero libero. Repellendus distinctio culpa iure modi odit accusantium!</p>
+        </div>
+    );
+};
+
+export default About;
